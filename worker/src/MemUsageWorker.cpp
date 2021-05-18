21c441af7a1c6af894d2042f72c605f5c09683b9
carl <479843162@qq.com>
2021-05-18 07:16:01 -0700

    VmemInfo

--- worker/src/MemUsageWorker.cpp
+++ worker/src/MemUsageWorker.cpp
@@ -29,25 +29,48 @@ bool dtop::worker::MemUsageWorker::handle_process(dtop::worker::ProfileQuery* qu
   if (!virtual_memory(&info)) {
     return false;
   }
-  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
-  auto used = (int64_t)info.used;
-  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
-  auto total = (int64_t)info.total;
-  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
-  std::cout << "used: " << used << std::endl;
-  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
-  std::cout << "total: " << total << std::endl;
-  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
-  auto mem_usage_message = MemUsageMessage();
-  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
-  mem_usage_message.set_max_mem(total);
-  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
-  mem_usage_message.set_used_mem(used);
-  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
-  reply->mutable_mem_usage_message()->set_used_mem(used);
-  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
-  reply->mutable_mem_usage_message()->set_max_mem(total);
-  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
+//  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
+//  auto used = (int64_t)info.used;
+//  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
+//  auto total = (int64_t)info.total;
+//  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
+//  std::cout << "used: " << used << std::endl;
+//  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
+//  std::cout << "total: " << total << std::endl;
+//  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
+//  auto mem_usage_message = MemUsageMessage();
+//  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
+//  mem_usage_message.set_max_mem(total);
+//  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
+//  mem_usage_message.set_used_mem(used);
+//  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
+//  reply->mutable_mem_usage_message()->set_used_mem(used);
+//  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
+//  reply->mutable_mem_usage_message()->set_max_mem(total);
+//  std::cout << __FILE__ << ": " << __LINE__ << std::endl;
+/**
+ *   int64 total = 1;
+  int64 available = 2;
+  int64 used = 3;
+  int64 free = 4;
+  int64 active = 5;
+  int64 inactive = 6;
+  int64 buffers = 7;
+  int64 cached = 8;
+  int64 wired = 9;
+ */
+
+	auto mut = reply->mutable_mem_usage_message();
+	mut->set_total(info.total);
+	mut->set_available(info.available);
+	mut->set_used(info.used);
+	mut->set_free(info.free);
+	mut->set_active(info.active);
+	mut->set_inactive(info.inactive);
+	mut->set_buffers(info.buffers);
+	mut->set_cached(info.cached);
+	mut->set_wired(info.wired);
+
   return true;
 }
 
